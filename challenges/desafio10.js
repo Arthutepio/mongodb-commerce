c0e1cd40e15c9617fe665492ed1aad6de1e67533
arthutepio <arthurodriguesr10@gmail.com>
2023-03-16 11:56:06 -0300

    fix: lint error

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,3 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "proteínas", "percentual": { $gt: 30, $lte: 40 } } }}, { _id: 0, nome: 1 })
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gt: 30, $lte: 40 } } } },
+  { _id: 0, nome: 1 });
